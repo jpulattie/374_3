258e8ec76a14e46c0eeec94bbb1d528a335d8a28
jpulattie <97932025+jpulattie@users.noreply.github.com>
2025-02-09 03:40:22 +0000

    perms

--- pulattij_assignment3.c
+++ pulattij_assignment3.c
@@ -44,7 +44,7 @@ struct movie *year_search(int search_year)
             sprintf(newFileName, "%d.txt", search_year);
             sprintf(newFilePath, "%s/%s", newDirectory, newFileName);
             fp = fopen(newFilePath, "w");
-            chmod(newFilePath, 0750);
+            chmod(newFilePath, 0640);
             found++;
         }
         if (current->year == search_year && found != 0)
